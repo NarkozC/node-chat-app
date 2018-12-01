dfc2242f3fa8fd250f43c22877f1648f624a3537
NarkozC <NarkozC@users.noreply.github.com>
2018-12-01 18:34:04 +0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,17 @@ var io = socketIO(server)
 io.on('connection', socket => {
   console.log('New User Connected')
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime()
+  })
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', newMessage => {
     console.log('Create Message', newMessage)
     io.emit('newMessage', {
@@ -19,6 +30,11 @@ io.on('connection', socket => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
